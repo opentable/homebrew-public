ed5967d11f17f11cd44de646aeef8c702d3b0baf
Judson <nyarly@gmail.com>
2017-06-26 17:46:02 -0700

    Release 0.5.16 of Sous

--- sous.rb
+++ sous.rb
@@ -3,10 +3,10 @@ class Sous < Formula
   homepage "https://github.com/opentable/sous"
 
   # When the version of Sous changes, these two fields need to be updated
-  version "0.5.15"
-  sha256 "932dda8e4355fb5eb41995ecc179e9fe165265ba277c61bf1bbabe07ad78e36f"
+  version "0.5.16"
+  sha256 "43fca7a1dd1e12b57ac4c18bff47d3bca31db522cd2526093dd20d46720f88f4"
 
-  url "https://github.com/opentable/sous/releases/download/0.5.15/sous-darwin-amd64_0.5.15.tar.gz"
+  url "https://github.com/opentable/sous/releases/download/0.5.16/sous-darwin-amd64_0.5.16.tar.gz"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
